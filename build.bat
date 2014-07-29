08b084ff4a5b6d6fe926b609a31152b0f1e26f07
Aaron C Spike <spikeac@mlc-wels.edu>
2014-07-29 12:38:27 -0500

    purge class files prior to each build; display additional warning information during compile

--- build.bat
+++ build.bat
@@ -1,3 +1,4 @@
-javac LabelPrinter.java
+del Label*.class
+javac -Xlint:unchecked LabelPrinter.java
 jar cvf LabelPrinter.jar Label*.class
 jarsigner -keystore keystore -signedjar sLabelPrinter.jar LabelPrinter.jar sign
