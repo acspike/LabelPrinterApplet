e5449ab9e3991c10f912e8756c3db3423d31be2f
Aaron C Spike <spikeac@mlc-wels.edu>
2014-07-29 12:45:07 -0500

    Add needed security properties to the manifest

--- build.bat
+++ build.bat
@@ -1,4 +1,5 @@
 del Label*.class
 javac -Xlint:unchecked LabelPrinter.java
 jar cvf LabelPrinter.jar Label*.class
+jar ufm LabelPrinter.jar addToManifest.txt
 jarsigner -tsa http://timestamp.digicert.com -keystore keystore -signedjar sLabelPrinter.jar LabelPrinter.jar sign
