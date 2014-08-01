44464afec977c8442e6952819b75c8a3eb90104a
Aaron C Spike <spikeac@mlc-wels.edu>
2014-08-01 07:39:53 -0500

    improved generic build command

--- build.bat
+++ build.bat
@@ -2,4 +2,4 @@ del Label*.class
 javac -Xlint:unchecked LabelPrinter.java
 jar cvf LabelPrinter.jar Label*.class
 jar ufm LabelPrinter.jar addToManifest.txt
-jarsigner -tsa http://timestamp.digicert.com -keystore keystore -signedjar sLabelPrinter.jar LabelPrinter.jar sign
+jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -digestalg SHA1 -verbose -strict -keystore signing_key.pfx -storetype pkcs12 -signedjar sLabelPrinter.jar LabelPrinter.jar signing_key
